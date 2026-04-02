935a3984daa513de616f5e7d3ca98ffa328fe444
Stephen Coffey <reaperskill@gmail.com>
2026-04-02 17:49:25 +0100

    Update actions.js
    Made the checkpoint faster

--- incremental/CavernousII/dist/script/actions.js
+++ incremental/CavernousII/dist/script/actions.js
@@ -676,7 +676,7 @@ const actions = [
     new Action("Create Hammer", 2500, [["Smithing", 1]], simpleCreate([["Iron Hammer", 1]]), simpleRequire([["Iron Bar", 1]])),
     new Action("Enter Barrier", 10000, [["Chronomancy", 1]], completeBarrier, startBarrier, null, barrierDuration),
     new Action("Sacrifice", 1000, [["Runic Lore", 1]], completeSacrifice),
-    new Action("Demonic Checkpoint", 10000, [["Runic Lore", 1]], completeMine, haveBloodmark),
+    new Action("Demonic Checkpoint", 1000, [["Runic Lore", 1]], completeMine, haveBloodmark),
     new Action("Exit", 50000000, [["Mining", 0.25], ["Woodcutting", 0.25], ["Magic", 0.25], ["Speed", 0.25], ["Smithing", 0.25], ["Runic Lore", 0.25], ["Combat", 0.25], ["Gemcraft", 0.25], ["Chronomancy", 0.25]], completeGame),
 ];
 function getAction(name) {
